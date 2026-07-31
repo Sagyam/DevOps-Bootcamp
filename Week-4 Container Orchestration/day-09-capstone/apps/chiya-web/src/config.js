d93fe39e0c3e76aba721ab9abced7de5f72bfe25
Sagyam Thapa <sagyamthapa32@gmail.com>
2026-07-31 14:25:58 +0545

    Remove capstone project

--- Week-4 Container Orchestration/day-09-capstone/apps/chiya-web/src/config.js
+++
@@ -1,6 +0,0 @@
-// Placeholder. In the cluster this file is replaced by a ConfigMap mount.
-window.CHIYA_CONFIG = {
-  ordersApi: "/api",
-  kitchenApi: "/api/kitchen",
-  tagline: "local dev build"
-};
